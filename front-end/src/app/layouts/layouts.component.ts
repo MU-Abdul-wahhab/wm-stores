148a711c07fffab1d5f66e7bc7fde2571e6f2ee3
Abdul_Malik <malik.info28@gmail.com>
2025-06-24 00:28:33 +0530

    test

---
+++ front-end/src/app/layouts/layouts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ShellComponent } from "./shell/shell.component";
+
+@Component({
+  selector: 'app-layouts',
+  imports: [ShellComponent],
+  templateUrl: './layouts.component.html',
+  styleUrl: './layouts.component.css'
+})
+export class LayoutsComponent {
+
+}
