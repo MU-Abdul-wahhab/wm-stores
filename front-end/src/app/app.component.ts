cb918907f392b1087948910293855e3d32c8f0c2
Abdul_Malik <malik.info28@gmail.com>
2025-06-24 16:28:06 +0530

    added auth UI

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthComponent } from "./features/auth/auth.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
