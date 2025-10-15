7782c57aa041feb3ecb4e9ed4ae302386efde694
Abdul_Malik <malik.info28@gmail.com>
2025-10-15 21:50:24 +0530

    added refresh token functionality

--- front-end/src/app/features/auth/login/login.component.ts
+++ front-end/src/app/features/auth/login/login.component.ts
@@ -1,9 +1,9 @@
-import { Component, DestroyRef, inject, signal } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
-import { RouterLink, Router, CanDeactivateFn } from '@angular/router';
-import { AuthService } from '../../../core/services/auth.service';
+import {Component, DestroyRef, inject, signal} from '@angular/core';
+import {ReactiveFormsModule, FormGroup, FormControl, Validators} from '@angular/forms';
+import {RouterLink, Router, CanDeactivateFn} from '@angular/router';
+import {AuthService} from '../../../core/services/auth.service';
 
-import { AlertComponent } from '../../../shared/alert/alert.component';
+import {AlertComponent} from '../../../shared/alert/alert.component';
 
 @Component({
   selector: 'app-login',
@@ -33,6 +33,7 @@ export class LoginComponent {
   get isEmailInvalid() {
     return (this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid);
   }
+
   get isPasswordInvalid() {
     return (this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid);
   }
@@ -43,13 +44,16 @@ export class LoginComponent {
       return;
     }
 
+    this.isLoading.set(true);
     const enteredEmail = this.form.value.email;
     const enteredPassword = this.form.value.password;
 
     if (enteredEmail && enteredPassword) {
 
-      const subscription = this.authService.signin({ email: enteredEmail, password: enteredPassword }).subscribe({
+      const subscription = this.authService.signin({email: enteredEmail, password: enteredPassword}).subscribe({
         next: (resData) => {
+          this.isLoading.set(false);
+          this.form.reset();
           this.onSuccessDialogClose();
         },
         error: (err) => {
@@ -61,14 +65,13 @@ export class LoginComponent {
 
       this.destroyRef.onDestroy(() => subscription.unsubscribe());
 
-      // this.form.reset();
       this.submitted.set(true);
     }
   }
 
   onSuccessDialogClose() {
     this.successMsg.set(undefined);
-    this.router.navigate(['/home'], { replaceUrl: true });
+    this.router.navigate(['/home'], {replaceUrl: true});
   }
 
 }
